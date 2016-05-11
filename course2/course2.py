834f338c9f7df43f478da631a3522392a9d1c6c0
Michael Dufour <mikerhinosfr@gmail.com>
2016-05-11 16:58:24 +0200

    Finished exercice 1

--- course2/course2.py
+++ course2/course2.py
@@ -1,7 +1,12 @@
 class Program():
     def __init__(self,*args,**kwargs):
         self.lang = input("What language ? : ")
-        self.version = float(input("What version ? : "))
+        self.version = float(input("Version ? : "))
         self.skill = input("What skill level ? : ")
+        
+    def upgrade(self):
+        new_version = float(input("What version ? : "))
+        print("We have updated the version for", self.lang)
+        self.version = new_version
         
 p1 = Program()
